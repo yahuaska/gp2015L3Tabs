39e8b8849eb3de2acc03988d0bb6361728b1895d
ringo@note <ringo.working@gmail.com>
2015-10-09 14:33:51 +0300

    init

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Главное окно прилжения.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Lab3Tabs
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Главное окно прилжения.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Лабораторная работа №3.
+//   Испольозвание ListBox и TabControl
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Lab3Tabs
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Лабораторная работа №3.
+    /// Испольозвание ListBox и TabControl
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}

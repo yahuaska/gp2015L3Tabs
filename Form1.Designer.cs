5ec02a497da7a665182a224be15da7f9ebc9cfb7
ringo@note <ringo.working@gmail.com>
2015-10-15 23:28:16 +0300

    finished

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Login = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonPasswords = new System.Windows.Forms.Button();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.MaskedTextBox();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.erroMsg = new System.Windows.Forms.Label();
+            this.Login.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Login.Controls.Add(this.tabPage1);
+            this.Login.Controls.Add(this.tabPage2);
+            this.Login.Location = new System.Drawing.Point(12, 12);
+            this.Login.Name = "Login";
+            this.Login.SelectedIndex = 0;
+            this.Login.Size = new System.Drawing.Size(531, 345);
+            this.Login.TabIndex = 0;
+            this.Login.Click += new System.EventHandler(this.TabControl1Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.erroMsg);
+            this.tabPage1.Controls.Add(this.buttonExit);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.buttonPasswords);
+            this.tabPage1.Controls.Add(this.buttonEnter);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.passwordBox);
+            this.tabPage1.Controls.Add(this.loginBox);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(523, 319);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Login";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.Location = new System.Drawing.Point(278, 290);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.Logout);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Пароли:";
+            this.label4.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "admin/admin/01.01.9999",
+            "user1/1resu/02.05.2018",
+            "user2/qwerty/10.11.2017",
+            "user3/qwerty/10.11.2014"});
+            this.listBox1.Location = new System.Drawing.Point(6, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(509, 147);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.Visible = false;
+            // 
+            // buttonPasswords
+            // 
+            this.buttonPasswords.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPasswords.Location = new System.Drawing.Point(359, 290);
+            this.buttonPasswords.Name = "buttonPasswords";
+            this.buttonPasswords.Size = new System.Drawing.Size(75, 23);
+            this.buttonPasswords.TabIndex = 6;
+            this.buttonPasswords.Text = "Пароли";
+            this.buttonPasswords.UseVisualStyleBackColor = true;
+            this.buttonPasswords.Visible = false;
+            this.buttonPasswords.Click += new System.EventHandler(this.ShowPasswords);
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonEnter.Location = new System.Drawing.Point(440, 290);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnter.TabIndex = 5;
+            this.buttonEnter.Text = "Вход";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.LoginButtonClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пароль";
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordBox.Location = new System.Drawing.Point(6, 90);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(509, 20);
+            this.passwordBox.SkipLiterals = false;
+            this.passwordBox.TabIndex = 3;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // loginBox
+            // 
+            this.loginBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginBox.Location = new System.Drawing.Point(6, 51);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(509, 20);
+            this.loginBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя пользователя:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(511, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Частное охранное предпритие";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(523, 319);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Котик";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Lab3Tabs.Properties.Resources.kotiki_kartinki5;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(517, 313);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // erroMsg
+            // 
+            this.erroMsg.AutoSize = true;
+            this.erroMsg.ForeColor = System.Drawing.Color.DarkRed;
+            this.erroMsg.Location = new System.Drawing.Point(10, 7);
+            this.erroMsg.Name = "erroMsg";
+            this.erroMsg.Size = new System.Drawing.Size(367, 13);
+            this.erroMsg.TabIndex = 11;
+            this.erroMsg.Text = "Неверное имя пользователя/пароль или истёк срок действия пароля.";
+            this.erroMsg.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.ClientSize = new System.Drawing.Size(555, 369);
+            this.Controls.Add(this.Login);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Login.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl Login;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonPasswords;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox passwordBox;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label erroMsg;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@
 
 namespace Lab3Tabs
 {
+    using System;
     using System.Windows.Forms;
 
     /// <summary>
@@ -19,6 +20,77 @@ namespace Lab3Tabs
         public Form1()
         {
             this.InitializeComponent();
+            this.buttonExit.Location = this.buttonEnter.Location;
+            this.buttonExit.Hide();
+            this.erroMsg.Location = label4.Location;
+        }
+
+        private void TabControl1Click(object sender, EventArgs e)
+        {
+            this.Login.SelectedIndex = 0;
+            MessageBox.Show(
+                "Ошибка доступа.",
+                "Ошибка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+        }
+
+        private void LoginButtonClick(object sender, EventArgs e)
+        {
+            var idx = this.listBox1.FindString(this.loginBox.Text);
+            if (idx < 0)
+            {
+                this.Logout();
+                this.loginBox.Focus();
+                this.erroMsg.Show();
+                return;
+            }
+
+            var loginData = this.listBox1.Items[idx].ToString().Split("/".ToCharArray(), 3);
+            var expirationDate = DateTime.Parse(loginData[2]);
+            if (this.loginBox.Text == loginData[0] && // Имя пользователя.
+                // Пароль.
+                this.passwordBox.Text == loginData[1] &&
+                // Дата истечения пароля.
+                expirationDate > DateTime.Today) 
+            {
+                this.erroMsg.Hide();
+                this.buttonEnter.Hide();
+                this.buttonExit.Show();
+                if (loginData[1] == "admin")
+                {
+                    this.buttonPasswords.Show();
+                }
+                else
+                {
+                    this.Login.SelectedIndex = 1;
+                }
+            }
+            else
+            {
+                this.Logout();
+                this.loginBox.Focus();
+                this.erroMsg.Show();
+            }
+
+        }
+
+        private void ShowPasswords(object sender, EventArgs e)
+        {
+            this.listBox1.Show();
+            this.label4.Show();
+        }
+
+        private void Logout(object sender = null, EventArgs e = null)
+        {
+            this.listBox1.Hide();
+            this.label4.Hide();
+            this.buttonPasswords.Hide();
+            this.buttonExit.Hide();
+            this.buttonEnter.Show();
+            this.loginBox.Text = string.Empty;
+            this.passwordBox.Text = string.Empty;
         }
     }
 }
